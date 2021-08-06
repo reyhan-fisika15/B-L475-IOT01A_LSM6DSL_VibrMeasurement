5a559608a8a99c8b294bb310774c1a0775b3bb05
reyhan-fisika15 <57849203+reyhan-fisika15@users.noreply.github.com>
2021-08-06 14:30:00 +0700

    Add files via upload

---
+++ Visual-CS_App/L475_VibrMeasurement/Form1.Designer.cs
@@ -0,0 +1,144 @@
+﻿
+namespace L475_VibrMeasurement
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.toolStrip1 = new System.Windows.Forms.ToolStrip();
+            this.toolStripLabel1 = new System.Windows.Forms.ToolStripLabel();
+            this.comboBox1 = new System.Windows.Forms.ToolStripComboBox();
+            this.btPortConnect = new System.Windows.Forms.ToolStripButton();
+            this.statusStrip1 = new System.Windows.Forms.StatusStrip();
+            this.toolStripStatusLabel1 = new System.Windows.Forms.ToolStripStatusLabel();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.formsPlot1 = new ScottPlot.FormsPlot();
+            this.toolStrip1.SuspendLayout();
+            this.statusStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // toolStrip1
+            // 
+            this.toolStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.toolStripLabel1,
+            this.comboBox1,
+            this.btPortConnect});
+            this.toolStrip1.Location = new System.Drawing.Point(0, 0);
+            this.toolStrip1.Name = "toolStrip1";
+            this.toolStrip1.Size = new System.Drawing.Size(800, 25);
+            this.toolStrip1.TabIndex = 0;
+            this.toolStrip1.Text = "toolStrip1";
+            // 
+            // toolStripLabel1
+            // 
+            this.toolStripLabel1.Name = "toolStripLabel1";
+            this.toolStripLabel1.Size = new System.Drawing.Size(60, 22);
+            this.toolStripLabel1.Text = "COM Port";
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(121, 25);
+            this.comboBox1.DropDown += new System.EventHandler(this.comboBox1_DropDown);
+            // 
+            // btPortConnect
+            // 
+            this.btPortConnect.BackColor = System.Drawing.Color.LightCoral;
+            this.btPortConnect.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Text;
+            this.btPortConnect.Image = ((System.Drawing.Image)(resources.GetObject("btPortConnect.Image")));
+            this.btPortConnect.ImageTransparentColor = System.Drawing.Color.Magenta;
+            this.btPortConnect.Margin = new System.Windows.Forms.Padding(5, 1, 5, 2);
+            this.btPortConnect.Name = "btPortConnect";
+            this.btPortConnect.Size = new System.Drawing.Size(56, 22);
+            this.btPortConnect.Text = "Connect";
+            this.btPortConnect.Click += new System.EventHandler(this.btPortConnect_Click);
+            // 
+            // statusStrip1
+            // 
+            this.statusStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.toolStripStatusLabel1});
+            this.statusStrip1.Location = new System.Drawing.Point(0, 428);
+            this.statusStrip1.Name = "statusStrip1";
+            this.statusStrip1.Size = new System.Drawing.Size(800, 22);
+            this.statusStrip1.TabIndex = 1;
+            this.statusStrip1.Text = "statusStrip1";
+            // 
+            // toolStripStatusLabel1
+            // 
+            this.toolStripStatusLabel1.Name = "toolStripStatusLabel1";
+            this.toolStripStatusLabel1.Size = new System.Drawing.Size(0, 17);
+            // 
+            // timer1
+            // 
+            this.timer1.Interval = 40;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // formsPlot1
+            // 
+            this.formsPlot1.BackColor = System.Drawing.Color.Transparent;
+            this.formsPlot1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.formsPlot1.Location = new System.Drawing.Point(0, 25);
+            this.formsPlot1.Name = "formsPlot1";
+            this.formsPlot1.Size = new System.Drawing.Size(800, 403);
+            this.formsPlot1.TabIndex = 2;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(96F, 96F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Dpi;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.formsPlot1);
+            this.Controls.Add(this.statusStrip1);
+            this.Controls.Add(this.toolStrip1);
+            this.Name = "Form1";
+            this.Text = "B-L475E-IOT01A Vibration Measurement App";
+            this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.Form1_FormClosing);
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.toolStrip1.ResumeLayout(false);
+            this.toolStrip1.PerformLayout();
+            this.statusStrip1.ResumeLayout(false);
+            this.statusStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ToolStrip toolStrip1;
+        private System.Windows.Forms.ToolStripLabel toolStripLabel1;
+        private System.Windows.Forms.ToolStripComboBox comboBox1;
+        private System.Windows.Forms.ToolStripButton btPortConnect;
+        private System.Windows.Forms.StatusStrip statusStrip1;
+        private System.Windows.Forms.ToolStripStatusLabel toolStripStatusLabel1;
+        private System.Windows.Forms.Timer timer1;
+        private ScottPlot.FormsPlot formsPlot1;
+    }
+}
+
---
+++ Visual-CS_App/L475_VibrMeasurement/Form1.cs
@@ -0,0 +1,131 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO.Ports;
+
+namespace L475_VibrMeasurement
+{
+    public partial class Form1 : Form
+    {
+        public SerialPort port;
+        public StringBuilder str;
+        public double[] dataAccelX;
+        public double[] dataAccelY;
+        public double[] dataAccelZ;
+        public int i = 0;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            // Create SerialPort object
+            port = new SerialPort();
+
+            // Register DataReceived event
+            port.DataReceived += new SerialDataReceivedEventHandler(SerialPort_DataReceived);
+
+            // Allocate memory for measurement data
+            // Data length is 10 secs (fs = 50 Hz)
+            dataAccelX = new double[500];
+            dataAccelY = new double[500];
+            dataAccelZ = new double[500];
+
+            // Allocate string builder
+            str = new StringBuilder();
+
+            formsPlot1.Plot.AddSignal(dataAccelX, 50, Color.Red);
+            formsPlot1.Plot.AddSignal(dataAccelY, 50, Color.Green);
+            formsPlot1.Plot.AddSignal(dataAccelZ, 50, Color.Blue);
+        }
+
+        private void comboBox1_DropDown(object sender, EventArgs e)
+        {
+            // Update available COM ports
+            comboBox1.Items.Clear();
+            comboBox1.Items.AddRange(SerialPort.GetPortNames());
+        }
+
+        private void btPortConnect_Click(object sender, EventArgs e)
+        {
+            // If previously connected.... then disconnect
+            if(port.IsOpen == false)
+            {
+                // Assign the port
+                port.PortName = comboBox1.Text;
+                // Assign the baud rate
+                port.BaudRate = 115200;
+                port.NewLine = "\r\n";
+
+                btPortConnect.Text = "Disconnect";
+                btPortConnect.BackColor = Color.LightGreen;
+                port.Open();
+
+                toolStripStatusLabel1.Text = "Connected to " + comboBox1.Text;
+                timer1.Start();
+            }
+            else
+            {
+                btPortConnect.Text = "Connect";
+                btPortConnect.BackColor = Color.LightCoral;
+                port.Close();
+                toolStripStatusLabel1.Text = "Disconnected";
+                timer1.Stop();
+            }
+        }
+
+        private void Form1_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            // Detach DataReceived event
+            port.DataReceived -= SerialPort_DataReceived;
+            // Close port connection
+            port.Close();
+        }
+
+        private void SerialPort_DataReceived(object sender, SerialDataReceivedEventArgs e)
+        { 
+            if(port.BytesToRead > 7)
+            {
+                // Get the data from B-L475E-IOT01A
+                str.Clear();
+                str.Append(port.ReadLine());
+
+                int portDataX = 0;
+                int portDataY = 0;
+                int portDataZ = 0;
+                double tmp;
+                String[] strtmp;
+
+                // Parse X, Y, Z value
+                char[] separators = new char[] { ',' };
+                strtmp = str.ToString().Split(separators);
+                Int32.TryParse(strtmp[0], out portDataX);
+                Int32.TryParse(strtmp[1], out portDataY);
+                Int32.TryParse(strtmp[2], out portDataZ);
+
+                // Update chart array
+                tmp = (double)portDataX / 32768.0 * 2.0;
+                dataAccelX[i % 500] = tmp; 
+                tmp = (double)portDataY / 32768.0 * 2.0;
+                dataAccelY[i % 500] = tmp; 
+                tmp = (double)portDataZ / 32768.0 * 2.0;
+                dataAccelZ[i % 500] = tmp;
+
+                i = (i + 1) % 500;
+            }
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            formsPlot1.Render();
+        }
+    }
+}
